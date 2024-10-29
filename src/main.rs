4e11c82652ebccb4ac2c4140b9c2b41b4bb0da85
Justin Johnson <justin.h.j.johnson@gmail.com>
2024-10-29 21:36:19 +1100

    rocket initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,43 @@ mod ao3;
 mod config;
 mod sftp;
 
-use ao3::series::Series;
-use ao3::work::Work;
-use ao3::common::DownloadFormat;
-use ao3::user::User;
-use config::read_config;
+use rocket::http::uri::Error;
+use rocket::tokio::io::split;
+use serde::Deserialize;
+use rocket::http::{Status, ContentType};
+use rocket::response::{status, Response};
+use rocket::serde::json::Json;
 
-use std::env::current_dir;
-use anyhow::{Ok, Result};
+#[macro_use] extern crate rocket;
 
-fn main() -> Result<()> {
-    let config = read_config();
+#[derive(Deserialize)]
+struct DownloadRequest<'r> {
+    url: &'r str
+}
 
-    let user: Option<User> = if let (Some(username), Some(password)) = (&config.ao3_username, &config.ao3_password) {
-        Some(User::new(username, password))
-    } else {
-        None
-    };
-    
-    let download_path = current_dir().unwrap().join("downloads");
-    let work = Work::parse_work("12", user.as_ref(), &config).unwrap();
-    let series = Series::parse_series("12345", user.as_ref(), &config).unwrap();
-    let _ = work.download(download_path.clone(), DownloadFormat::EPUB, false, &String::new());
-    let _ = series.download(download_path.clone(), DownloadFormat::EPUB);
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
-    println!("{}", series);
-    println!("{}", work);
+#[post("/download", format = "json", data = "<request>")]
+fn download(request: Json<DownloadRequest<'_>>) -> (Status, &'static str) {
+    let split_url: Vec<&str> = request.url.split('/').collect();
+    println!("{:?}", split_url);
 
-    Ok(())
+    if split_url.len() > 5 {
+        return (Status::BadRequest, "That was a dogshit request")
+    }
+
+    let url_type = *split_url.get(3).unwrap();
+    let id = *split_url.get(4).unwrap();
+    println!("type: {}, id: {}", url_type, id);
+    (Status::Ok, "")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![download])
 }
